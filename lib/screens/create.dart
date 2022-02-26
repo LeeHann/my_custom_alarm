8a6e5d2ac56a401b0e70b4572271d144c87cba11
LeeHann <0104ihn@naver.com>
2022-02-26 18:41:46 +0900

    set up

---
+++ lib/screens/create.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreateAlarm extends StatelessWidget {
+  const CreateAlarm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_custom_alarm/screens/create.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center),
+      ),
+      floatingActionButton: FloatingActionButton( // 알람 추가 버튼 create를 테스트하기 위해 작성함 - column 안에 넣는 등 레이아웃에 넣어서 배열하면 좋을 듯
+        onPressed: () {
+          Navigator.push(context,
+                  CupertinoPageRoute(builder: (context) => CreateAlarm()))
+              .then((value) {
+            setState(() {});
+          });
+        },
+        tooltip: '알람을 추가하세요.',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
