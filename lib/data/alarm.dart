48077c2e8191c839d96023d6f0f732c331b18377
dampers <houma757@gmail.com>
2022-03-08 11:20:02 +0900

    write alarm.dart

---
+++ lib/data/alarm.dart
@@ -0,0 +1,29 @@
+import '../DB/db.dart';
+
+class testAlarm{
+  int id;
+  DateTime? _settingTime;
+  List<bool> _days = [false, false, false, false, false, false, false];
+  DateTime? _settingDay; // 알람이 울리는 특정일
+  String? alarmName;
+  bool _isSound = false, _isVib = false, _isRepeat = false;
+
+  testAlarm({required this.id, required alarmName});
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnAlarmName: alarmName
+    };
+    if(id != null){
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+  fromMap(Map<String, dynamic> map) {
+    id = map[columnId];
+    alarmName = map[columnAlarmName];
+    print('$id $alarmName');
+  }
+
+}
